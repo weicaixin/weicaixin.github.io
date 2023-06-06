863ee8af69232a23b07f63b8f921e4480356a119
weicaixin <weicaixin@qq.com>
2023-06-06 16:26:58 +0800

    ...

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -26,3 +26,39 @@ function sidebarList() {
     '/烹饪书/': getSidebar({ contentRoot: '/docs', contentDirs: ['烹饪书'], collapsible: true, collapsed: true }),
   }
 }
+
+// function toSidebarOption(tree = []) {
+//   if (!Array.isArray(tree)) return [];
+
+//   return tree.map((v) => {
+//     if (v.children !== undefined) {
+//       return {
+//         text: v.name,
+//         collapsible: true,
+//         collapsed: true,
+//         items: toSidebarOption(v.children),
+//       };
+//     } else {
+//       return {
+//         text: v.name.replace(".md", ""),
+//         link: v.path.split("docs")[1].replace(".md", ""),
+//       };
+//     }
+//   });
+// }
+
+// function getSiderbar(srcPath, title) {
+//   const srcDir = dirTree(srcPath, {
+//     extensions: /\.md$/,
+//     normalizePath: true,
+//   });
+
+//   return [
+//     {
+//       text: title == undefined ? srcDir.name : title,
+//       collapsible: true,
+//       collapsed: true,
+//       items: toSidebarOption(srcDir.children),
+//     },
+//   ];
+// }
